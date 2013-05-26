d7e85e8121c1166a2afe1a2e643be11a077d306b
Mikhail Polgun <mikhailpolgun@gmail.com>
2013-05-26 10:11:15 +0400

    Added procedure to find the farthest points

--- Polgun.ComputationGeometry.Tests/PointsDistancesTests.cs
+++ Polgun.ComputationGeometry.Tests/PointsDistancesTests.cs
@@ -9,6 +9,7 @@ namespace Polgun.ComputationGeometry.Tests
     [TestFixture]
     public class PointsDistancesTests
     {
+        #region FindClosestPoint Tests
         [Test]
         [ExpectedException(typeof(ArgumentNullException))]
         public void TestUnitOfWork()
@@ -26,9 +27,9 @@ namespace Polgun.ComputationGeometry.Tests
         [Test]
         public void TestFindClosestInOnePoint()
         {
-            Point point = new Point(100,100);
+            Point point = new Point(100, 100);
 
-            FindPairResult result = PointsDistances.FindClosestPair(new[] {point});
+            FindPairResult result = PointsDistances.FindClosestPair(new[] { point });
 
             Assert.That(result.Point1, Is.EqualTo(point));
             Assert.That(result.Point2, Is.EqualTo(point));
@@ -77,13 +78,130 @@ namespace Polgun.ComputationGeometry.Tests
 
             AssertResult(expectedResult, result);
 
+        } 
+        #endregion
+
+        #region FindFarthestPoint Tests
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestFindFarthestInNullList()
+        {
+            PointsDistances.FindFarthestPair(null);
         }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestFindFarthestInEmptyList()
+        {
+            PointsDistances.FindFarthestPair(new Point[0]);
+        }
+
+        [Test]
+        public void TestFindFarthestInOnePoint()
+        {
+            Point point = new Point(100, 100);
+
+            FindPairResult result = PointsDistances.FindFarthestPair(new[] { point });
+
+            Assert.That(result.Point1, Is.EqualTo(point));
+            Assert.That(result.Point2, Is.EqualTo(point));
+            Assert.That(result.Distance, Is.EqualTo(0.0));
+        }
+
+        [Test]
+        public void TestFindFarthestInTwoPints()
+        {
+            Point point1 = new Point(10, 0);
+            Point point2 = new Point(20, 0);
+
+            FindPairResult result = PointsDistances.FindFarthestPair(new[] { point1, point2 });
+
+            Assert.That(result.Point1, Is.EqualTo(point1));
+            Assert.That(result.Point2, Is.EqualTo(point2));
+            Assert.That(result.Distance, Is.EqualTo(10.0));
+        }
+
+        [Test]
+        public void TestFindFarthestInThreePoints()
+        {
+            Point point1 = new Point(10, 0);
+            Point point2 = new Point(20, 0);
+            Point point3 = new Point(100, 100);
+
+            FindPairResult result = PointsDistances.FindFarthestPair(new[] { point1, point2, point3 });
+
+            Assert.That(result.Point1, Is.EqualTo(point1));
+            Assert.That(result.Point2, Is.EqualTo(point3));
+        }
+
+        [Test]
+        public void TestFindFarthestInTenPoints()
+        {
+            List<Point> points = Enumerable.Range(0, 10)
+                                           .Select(a => new Point(-1 * a * a, -1 * a * a))
+                                           .ToList();
+            FindPairResult expectedResult = new FindPairResult(points.First(), points.Last());
+
+            FindPairResult result = PointsDistances.FindFarthestPair(points);
+
+            AssertResult(expectedResult, result);
+
+        } 
+        
+
+        #endregion
 
         private void AssertResult(FindPairResult expected, FindPairResult actual)
         {
-            Assert.That(actual.Point1, Is.EqualTo(expected.Point1));
-            Assert.That(actual.Point2, Is.EqualTo(expected.Point2));
+            //Assert.That(actual.Point1, Is.EqualTo(expected.Point1));
+            //Assert.That(actual.Point2, Is.EqualTo(expected.Point2));
+            AssertMinimalPoints(expected, actual);
+            AssertMaximalPoints(expected, actual);
             Assert.That(actual.Distance, Is.EqualTo(expected.Distance));
+        }
+
+        private void AssertMaximalPoints(FindPairResult expected, FindPairResult actual)
+        {
+            Point expectedMax = Max(expected.Point1, expected.Point2);
+            Point actualMax = Max(actual.Point1, actual.Point2);
+
+            Assert.That(actualMax, Is.EqualTo(expectedMax));
+        }
+
+        private void AssertMinimalPoints(FindPairResult expected, FindPairResult actual)
+        {
+            Point expectedMin = Min(expected.Point1, expected.Point2);
+            Point actualMin = Min(actual.Point1, actual.Point2);
+
+            Assert.That(actualMin, Is.EqualTo(expectedMin));
+        }
+
+        private Point Min(Point lvalue, Point rvalue)
+        {
+              if (lvalue.X < rvalue.X)
+            {
+                return lvalue;
+            }
+            else if (lvalue.X == rvalue.X)
+            {
+                if (lvalue.Y < rvalue.Y || lvalue.Y == rvalue.Y)
+                    return lvalue;
+            }
+            return rvalue;
+        }
+
+        private Point Max(Point lvalue, Point rvalue)
+        {
+            if (lvalue.X < rvalue.X)
+            {
+                return rvalue;
+            }
+            else if (lvalue.X == rvalue.X)
+            {
+                if (lvalue.Y < rvalue.Y || lvalue.Y == rvalue.Y)
+                    return rvalue;
+            }
+            return lvalue;
         }
 
         private FindPairResult BruteForceClosestPoints(IList<Point> points)
--- Polgun.ComputationGeometry/PointsDistances.cs
+++ Polgun.ComputationGeometry/PointsDistances.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Linq;
 
 namespace Polgun.ComputationGeometry
 {
@@ -30,6 +31,28 @@ namespace Polgun.ComputationGeometry
                 Point point1;
                 Point point2;
                 FindClosestPoints(pnts, ySeries, out point1, out point2);
+                return new FindPairResult(point1, point2);
+            }
+        }
+
+        public static FindPairResult FindFarthestPair(IList<Point> points)
+        {
+            Contract.Requires<ArgumentNullException>(points != null, "points");
+            Contract.Requires<ArgumentOutOfRangeException>(points.Count > 0);
+
+            if (points.Count == 1)
+            {
+                return new FindPairResult(points[0], points[0]);
+            }
+            else if (points.Count == 2)
+            {
+                return new FindPairResult(points[0], points[1]);
+            }
+            else // Срабатывает алгоритм расчета
+            {
+                Point point1;
+                Point point2;
+                RotatingCalipers(points, out point1, out point2);
                 return new FindPairResult(point1, point2);
             }
         }
@@ -135,12 +158,118 @@ namespace Polgun.ComputationGeometry
             }
         }
 
+        private static void RotatingCalipers(IList<Point> pts, out Point point1, out Point point2)
+        {
+            Point[][] l_u = ConvexHull_LU(pts.ToArray());
+            Point[] lower = l_u[0];
+            Point[] upper = l_u[1];
+            int i = 0;
+            int j = lower.Length - 1;
+
+            List<PointPair> pairs = new List<PointPair>(lower.Length + upper.Length);
+
+            while (i < upper.Length - 1 || j > 0)
+            {
+                pairs.Add(new PointPair(upper[i], lower[j]));
+
+                if (i == upper.Length - 1) j--;
+                else if (j == 0) i += 1;
+                else if ((upper[i + 1].Y - upper[i].Y) * (lower[j].X - lower[j - 1].X) >
+                    (lower[j].Y - lower[j - 1].Y) * (upper[i + 1].X - upper[i].X))
+                    i++;
+                else
+                    j--;
+            }
+
+            double distance = SquareDistance(pairs[0].Point1, pairs[0].Point2);
+            point1 = pairs[0].Point1;
+            point2 = pairs[0].Point2;
+
+            for (int index = 1; index < pairs.Count; ++index)
+            {
+                var pair = pairs[i];
+                double d = SquareDistance(pair.Point1, pair.Point2);
+                if (d > distance)
+                {
+                    distance = d;
+                    point1 = pair.Point1;
+                    point2 = pair.Point2;
+                }
+            }
+        }
+
+        private static Point[][] ConvexHull_LU(Point[] arr_pts)
+        {
+            var u = new List<Point>();
+            var l = new List<Point>();
+            var pts = new List<Point>(arr_pts.Length);
+            pts.AddRange(arr_pts);
+            pts.Sort(Compare);
+            foreach (Point p in pts)
+            {
+                while (u.Count > 1 && Orientation(At(u, -2), At(u, -1), p) <= 0) Pop(u);
+                while (l.Count > 1 && Orientation(At(l, -2), At(l, -1), p) >= 0) Pop(l);
+                u.Add(p);
+                l.Add(p);
+            }
+            return new Point[][] { l.ToArray(), u.ToArray() };
+        }
+
+        private static T At<T>(List<T> l, int index)
+        {
+            int n = l.Count;
+            if (index < 0)
+                return l[n + index];
+            return l[index];
+        }
+
+        private static double Orientation(Point p, Point q, Point r)
+        {
+            return (q.Y - p.Y) * (r.X - p.X) - (q.X - p.X) * (r.Y - p.Y);
+        }
+
+        private static void Pop<T>(List<T> l)
+        {
+            int n = l.Count;
+            l.RemoveAt(n - 1);
+        }
+
+
+        private static int Compare(Point a, Point b)
+        {
+            if (a.X < b.X)
+            {
+                return -1;
+            }
+            else if (a.X == b.X)
+            {
+                if (a.Y < b.Y)
+                    return -1;
+                else if (a.Y == b.Y)
+                    return 0;
+            }
+            return 1;
+        }
+
         internal static double SquareDistance(Point p1, Point p2)
         {
             double dx = p1.X - p2.X;
             double dy = p1.Y - p2.Y;
             return dx * dx + dy * dy;
 
-        } 
+        }
+
+        private struct PointPair
+        {
+            public PointPair(Point point1, Point point2)
+                : this()
+            {
+                Point1 = point1;
+                Point2 = point2;
+            }
+
+            public Point Point1 { get; private set; }
+            public Point Point2 { get; private set; }
+        }
     }
 }
